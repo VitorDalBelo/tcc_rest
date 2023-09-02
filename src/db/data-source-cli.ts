9c5f279ce4f297039de41beadcdfd8ddff900f3c
VitorDalBelo <vitoralmeida721@gmail>
2023-09-02 15:33:01 -0300

    ssl_deploy2

--- src/db/data-source-cli.ts
+++ src/db/data-source-cli.ts
@@ -10,7 +10,9 @@ const dataSourceOptions: DataSourceOptions ={
     database:process.env.DB_NAME,
     entities:[__dirname + '/../**/*.entity{.js,ts}'],
     migrations:[__dirname + '/migrations/*.{.js,ts}'],
-    ssl: false
+    ssl: {
+        rejectUnauthorized: false, // Isso desativa a verificação de certificado SSL. Use com cautela!
+    }
 }
 
 const dataSource = new DataSource(dataSourceOptions);
